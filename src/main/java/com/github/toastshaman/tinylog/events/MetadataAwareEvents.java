a99dcbae85ece165f52f102d084d9a569c4fac0c
Kevin Denver <kevin.denver@gmail.com>
2022-08-04 09:28:14 +0100

    MetadataAwareEvents

--- src/main/java/com/github/toastshaman/tinylog/events/MetadataAwareEvents.java
+++ src/main/java/com/github/toastshaman/tinylog/events/MetadataAwareEvents.java
@@ -8,6 +8,7 @@ import com.github.toastshaman.tinylog.StructuredLogs;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Objects;
 
 public class MetadataAwareEvents implements Events {
 
@@ -15,19 +16,20 @@ public class MetadataAwareEvents implements Events {
     private final StructuredLogs structuredLogs;
 
     public MetadataAwareEvents(StructuredLogs structuredLogs, Events... events) {
-        this.structuredLogs = structuredLogs;
-        this.delegates = new ArrayList<>(Arrays.stream(events).toList());
+        this.structuredLogs = Objects.requireNonNull(structuredLogs);
+        this.delegates = new ArrayList<>(Arrays.stream(Objects.requireNonNull(events)).toList());
     }
 
     @Override
     public void log(Event event) {
+        Objects.requireNonNull(event);
         MetadataEvent eventWithMetadata = new MetadataEvent(structuredLogs.apply(event), event);
         delegates.forEach(it -> it.log(eventWithMetadata));
     }
 
     @Override
     public Events then(Events events) {
-        delegates.add(events);
+        delegates.add(Objects.requireNonNull(events));
         return this;
     }
 }
